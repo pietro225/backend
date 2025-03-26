58de7edcc1700d033be06760eb89af9dc5bd7e66
pietro225 <pietro.zanchi@escola.pr.gov.br>
2025-03-26 13:51:42 +0000

    Aula01Variaveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class Aula01Variavel {
+
+    public static void main(string[] args)  {
+       //variavel logica 
+
+       //tipo primitivo
+       boolean valorlogicoPrimitivo = true;
+
+       //tipo abstrato
+       boolean valorogicoAbstrato = false;
+    }
+}

8e1317e80cf91f4cc66b286d9bb804014c615600
pietro225 <pietro.zanchi@escola.pr.gov.br>
2025-03-28 13:50:36 +0000

    aula 02 de variavel

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -16,5 +16,34 @@ public class Aula01Variavel {
 
        //Exibe o texto
        System.out.println("O valor da variavel alorogicoAbstrato é: " + valorogicoAbstrato);
+
+       //variaveis inteiras
+
+       //tipo primitivo
+       int valorInteiro = 10; // 32bits
+       long valorInteiroLongo =10l; // 64bits
+
+       //tipo abstrato
+       Integer valorInteiroAbs = 20;
+       Long valorLongoAbs = 20L;
+
+       System.out.println("O valor do valorInteiro é: " + valorInteiro + ", e o valor de valorInteiroLongo é: " + valorInteiroLongo);
+
+       //variaveis Decimais
+
+       //tipo primitivo
+       float numeroFloat = 10.5f; // 32bits
+       double numeroDouble=10.5; // 64bits
+
+       //tipo abstrato
+       float numeroFloatAbs = 10.5f;
+       double numeroDoubleAbs=10.5;
+       
+       
+
+       System.out.println("O valor do numeroFloat é: " + numeroFloat + ", e o valor de numeroDouble é: " + numeroDouble);
+
+
+
     }
 }

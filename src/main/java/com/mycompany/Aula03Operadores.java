b6c5d219fc9f631e15d2af80060634cb46588645
pietro225 <pietro.zanchi@escola.pr.gov.br>
2025-04-02 19:41:18 +0000

    aufuiwhfc

---
+++ src/main/java/com/mycompany/Aula03Operadores.java
@@ -0,0 +1,20 @@
+package com.mycompany;
+
+public class Aula03Operadores {
+    public static void main(String[] args) {
+        /**
+         * tipos de Comentários 
+         */
+
+         //Comentários de uma linha   
+         
+         /**
+          * Comentários
+          * de multiplas 
+          * linhas
+          */
+
+    }
+
+    
+}
